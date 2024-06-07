bc92131545e84178e2e2e23d7b031d29dacdd5bf
Ebube <139717141+Ehieduebube@users.noreply.github.com>
2024-06-07 05:17:21 -0700

    Update Weather.jsx

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import '../Css/style.css'
+import '../Css/Style.css'
 const api = {
   key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
   base: "https://api.openweathermap.org/data/2.5/"
